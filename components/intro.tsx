21e18431077efed32c28de210a570eaec9f413d6
Ridwan <rhaque34@gatech.edu>
2025-07-01 12:48:27 -0400

    Text Description update

--- components/intro.tsx
+++ components/intro.tsx
@@ -38,7 +38,7 @@ export default function Intro() {
         </div>
         <p className="mb-10 mt-4 text-2xl font-medium leading-[1.5] sm:text-4xl">
             <span className="font-bold">Hello, I'm Ridwan.</span> I'm a{" "}
-            <span className="font-bold">full-stack developer</span> with{" "}
+            <span className="font-bold">Computer Engineering Student</span> with{" "}
             <span className="font-bold">2 years</span> of experience. I enjoy
             building <span className="italic">sites & apps</span>. My focus is{" "}
             <span className="underline">React (Next.js)</span>.
