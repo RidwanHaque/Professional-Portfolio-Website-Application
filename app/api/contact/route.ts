bc29ec2064abf3ca5343953319a67faa920b3282
Ridwan <rhaque34@gatech.edu>
2025-12-16 23:14:19 -0500

    fixed contact api

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,18 +1,18 @@
 import { Resend } from "resend";
-import { ContactFormEmail } from "@/email/contact-form-email";
+import ContactFormEmail from "@/email/contact-form-email";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const POST = async (request: Request) => {
   const body = await request.json();
-  const { email, message, name } = body;
+  const { senderEmail, message } = body;
 
   try {
     const data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: email,
+      to: senderEmail,
       subject: "New message from contact form",
-      react: ContactFormEmail({ email, message, name }),
+      react: ContactFormEmail({ senderEmail, message }),
     });
 
     return Response.json({ data });
